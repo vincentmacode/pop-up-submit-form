065f9d4fa26742e6c1a694ac346a7ede06ce1aaf
Vincent Ma <vincentma.code@gmail.com>
2025-12-01 11:37:05 -0500

    Added loading animation in form
    A loading animation shows up when user hits accept.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
 
 
 
@@ -9,4 +11,16 @@ setTimeout(function(){
 
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
+})
+
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="./image/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>`
+    
 })
